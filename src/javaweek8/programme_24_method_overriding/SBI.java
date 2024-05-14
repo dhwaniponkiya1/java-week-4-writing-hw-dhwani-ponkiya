60296e655bd5d1145054af67417461ca060b8e81
dhwaniponkiya <dhwaniponkiya20@gmail.com>
2024-05-14 14:32:42 +0100

    writing hw done

---
+++ src/javaweek8/programme_24_method_overriding/SBI.java
@@ -0,0 +1,11 @@
+package javaweek8.programme_24_method_overriding;
+
+/**
+ * Creating child classes.
+ */
+public class SBI extends Bank {
+    public int getRateOfInterest() {
+        return 8;
+    }
+}
+
